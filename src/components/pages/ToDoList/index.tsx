30f39ac6689710ce6bfae1a226adb1b3e401dc39
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-15 00:39:18 -0300

    Add ToDoList Page

---
+++ src/components/pages/ToDoList/index.tsx
@@ -0,0 +1,13 @@
+import Container from '../../layout/Container';
+
+import { Content } from './styles';
+
+export function ToDoList() {
+  return (
+    <Content>
+      <Container>
+        <h1>Login Realizado!</h1>
+      </Container>
+    </Content>
+  );
+}
