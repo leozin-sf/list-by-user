4f09eaf5126e70a0b7fb344716073d28a52e6cf3
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-15 00:38:42 -0300

    Add Supabase Component

--- src/components/pages/index/types.ts
+++
@@ -1,6 +0,0 @@
-export function supaString(input: string | undefined): string {
-  if (input === undefined) {
-    throw new Error('Variável de ambiente necessária não definida.');
-  }
-  return input;
-}
