a4ed6d38a6b4cbde804a86e4aa21d88317da3e1e
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-15 23:17:43 -0300

    PageWrapper update

--- src/components/layout/Page/styles.ts
+++ src/components/layout/Page/styles.ts
@@ -4,8 +4,4 @@ import styled from '@emotion/styled';
 export const PageWrapper = styled.div`
   background-color: ${(p) => p.theme.colors.loginPage.background};
   min-height: 100vh;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
 `;
