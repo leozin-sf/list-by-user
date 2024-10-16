1712e8e0268b427efa2461ccf747a3432c39e450
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-15 23:18:30 -0300

    Add menu with signOut

--- src/components/pages/ToDoList/index.tsx
+++ src/components/pages/ToDoList/index.tsx
@@ -1,11 +1,31 @@
+import { supabase } from '../../auth/SupaBaseClient';
+import { useNavigate } from 'react-router-dom';
+
 import Container from '../../layout/Container';
 
-import { Content } from './styles';
+import { Content, Menu } from './styles';
 
 export function ToDoList() {
+  const navigate = useNavigate();
+
+  const signOutApp = async () => {
+    try {
+      const { error } = await supabase.auth.signOut();
+
+      if (error) {
+        console.log(error);
+      } else {
+        navigate('/');
+      }
+    } catch (error) {}
+  };
+
   return (
     <Content>
       <Container>
+        <Menu>
+          <button onClick={signOutApp}>Logout</button>
+        </Menu>
         <h1>Login Realizado!</h1>
       </Container>
     </Content>
--- src/components/pages/ToDoList/styles.ts
+++ src/components/pages/ToDoList/styles.ts
@@ -1,3 +1,7 @@
 import styled from '@emotion/styled';
 
 export const Content = styled.div``;
+
+export const Menu = styled.div`
+  display: flex;
+`;
