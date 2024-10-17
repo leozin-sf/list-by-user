50d905d2f772c827ca8f4fd1a030148aa7b74617
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-17 00:20:40 -0300

    Add task fetching and handleTask

--- src/components/pages/ToDoList/index.tsx
+++ src/components/pages/ToDoList/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState, useRef } from 'react';
 import { supabase } from '../../auth/SupaBaseClient';
 import { useNavigate } from 'react-router-dom';
 
@@ -13,12 +13,72 @@ import {
   UserNameText,
 } from './styles';
 
-import { Task } from './types';
+import { TaskTypes } from './types';
+
+const getUserId = async () => {
+  try {
+    const {
+      data: { user },
+      error,
+    } = await supabase.auth.getUser();
+    if (error) {
+      console.log('Erro on getUser', error);
+      return null;
+    }
+    if (user) {
+      console.log('ID do usuário', user.id);
+      return user.id;
+    } else {
+      console.log('Usuário não autenticado');
+      return null;
+    }
+  } catch (error) {}
+};
 
 export function ToDoList() {
   const navigate = useNavigate();
   const [userName, setUserName] = useState<string>('');
-  const [tasks, setTasks] = useState<Task[]>([]);
+  const [tasks, setTasks] = useState<TaskTypes[]>([]);
+  const newTaskText = useRef<HTMLInputElement>(null);
+
+  const getTasks = async (userID: string) => {
+    const { data: tasksData, error: tasksError } = await supabase
+      .from('to_do_list')
+      .select('*')
+      .eq('id', userID)
+      .order('created_at', { ascending: false });
+
+    if (tasksError) {
+      console.log(tasksError);
+    } else {
+      setTasks(tasksData);
+    }
+  };
+
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const userID = await getUserId();
+
+        if (userID) {
+          const { data: userData, error: userError } = await supabase
+            .from('users')
+            .select('*')
+            .eq('id', userID)
+            .single();
+          if (userError) {
+            console.log(userError);
+            return;
+          }
+
+          setUserName(userData.name);
+          getTasks(userID);
+        }
+      } catch (error) {}
+    };
+
+    fetchUserData();
+  }, []);
 
   const signOutApp = async () => {
     try {
@@ -32,34 +92,27 @@ export function ToDoList() {
     } catch (error) {}
   };
 
-  useEffect(() => {
-    const getUserId = async () => {
-      try {
-        const {
-          data: { user },
-          error,
-        } = await supabase.auth.getUser();
-        if (error) {
-          console.log(error);
-        } else if (user) {
-          const { data: users, error: errorUsers } = await supabase
-            .from('users')
-            .select('name')
-            .eq('id', user.id)
-            .single();
-          if (errorUsers) {
-            console.log(errorUsers);
-          } else {
-            setUserName(users.name);
-          }
-        } else {
-          console.log('User not found');
-        }
-      } catch (error) {}
-    };
+  const handleAddTask = async (e: FormEvent) => {
+    e.preventDefault();
+    const userID = await getUserId();
 
-    getUserId();
-  }, []);
+    if (userID && newTaskText.current?.value) {
+      const { data, error } = await supabase.from('to_do_list').insert([
+        {
+          text: newTaskText.current.value,
+          task_confirmed: false,
+          id: userID,
+        },
+      ]);
+
+      if (error) {
+        console.log('Erro em todolist, erro');
+      } else {
+        getTasks(userID);
+        newTaskText.current.value = '';
+      }
+    }
+  };
 
   return (
     <Content>
@@ -72,6 +125,15 @@ export function ToDoList() {
           <LogoutButton onClick={signOutApp}>Logout</LogoutButton>
         </Menu>
         <h1>Login Realizado!</h1>
+        <form>
+          <input type="text" placeholder="Adicionar tarefa" ref={newTaskText} />
+          <button onClick={handleAddTask}>Adicionar</button>
+        </form>
+        <ul>
+          {tasks.map((task) => (
+            <li key={task.id}>{task.text}</li>
+          ))}
+        </ul>
       </Container>
     </Content>
   );
