d00b815dfd29f5cf8b11d872cd3bbe07070a1a0d
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-16 22:25:52 -0300

    Add userName on Menu

--- src/components/pages/ToDoList/index.tsx
+++ src/components/pages/ToDoList/index.tsx
@@ -1,12 +1,21 @@
+import { useEffect, useState } from 'react';
 import { supabase } from '../../auth/SupaBaseClient';
 import { useNavigate } from 'react-router-dom';
 
 import Container from '../../layout/Container';
 
-import { Content, Menu } from './styles';
+import {
+  Content,
+  Menu,
+  LogoutButton,
+  Wellcome,
+  WellcomeText,
+  UserNameText,
+} from './styles';
 
 export function ToDoList() {
   const navigate = useNavigate();
+  const [userName, setUserName] = useState<string>('');
 
   const signOutApp = async () => {
     try {
@@ -20,11 +29,36 @@ export function ToDoList() {
     } catch (error) {}
   };
 
+  useEffect(() => {
+    const fetchUserName = async () => {
+      const userEmail = localStorage.getItem('user_email');
+      if (userEmail) {
+        const { data, error } = await supabase
+          .from('users')
+          .select('name')
+          .eq('email', userEmail)
+          .single();
+
+        if (error) {
+          console.log(error);
+        } else {
+          setUserName(data.name);
+        }
+      }
+    };
+
+    fetchUserName();
+  }, []);
+
   return (
     <Content>
       <Container>
         <Menu>
-          <button onClick={signOutApp}>Logout</button>
+          <Wellcome>
+            <WellcomeText>Olá,</WellcomeText>{' '}
+            <UserNameText>{userName}</UserNameText>
+          </Wellcome>
+          <LogoutButton onClick={signOutApp}>Logout</LogoutButton>
         </Menu>
         <h1>Login Realizado!</h1>
       </Container>
--- src/components/pages/ToDoList/styles.ts
+++ src/components/pages/ToDoList/styles.ts
@@ -3,5 +3,26 @@ import styled from '@emotion/styled';
 export const Content = styled.div``;
 
 export const Menu = styled.div`
+  height: 3.625rem;
   display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const LogoutButton = styled.button`
+  height: 2rem;
+`;
+
+export const Wellcome = styled.div``;
+
+export const WellcomeText = styled.span`
+  font-size: 1.3rem;
+  font-weight: 300;
+  color: ${(p) => p.theme.colors.white};
+`;
+
+export const UserNameText = styled.span`
+  font-size: 1.3rem;
+  font-weight: bold;
+  color: ${(p) => p.theme.colors.toDoListPage.userText};
 `;
