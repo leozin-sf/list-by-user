6ba15eab821fc20401337bed8b822cdb846ae1dd
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-24 16:51:38 -0300

    Add onKeyDown event

--- src/components/pages/ToDoList/index.tsx
+++ src/components/pages/ToDoList/index.tsx
@@ -68,7 +68,6 @@ export function ToDoList() {
   const filteredTasks = tasks.filter(
     (task) => task.task_confirmed === showCompleted
   );
-  console.log(filteredTasks);
   const tasksPerPage = 10;
   const indexOfLastTask = currentPage * tasksPerPage;
   const indexOfFirstTask = indexOfLastTask - tasksPerPage;
@@ -353,6 +352,11 @@ export function ToDoList() {
                           [task.list_id]: e.target.value,
                         }))
                       }
+                      onKeyDown={(e) => {
+                        if (e.key === 'Enter') {
+                          handleUpdateTask(task.list_id);
+                        }
+                      }}
                     />
                     <SaveButton onClick={() => handleUpdateTask(task.list_id)}>
                       Salvar
