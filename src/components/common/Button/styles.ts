f8528ff241f1680d83b82a62b0175fd361a50ed9
Leonardo Silva <leonardosilvaferreira@hotmail.com>
2024-10-28 02:02:37 -0300

    Desktop and mobile update

--- src/components/common/Button/styles.ts
+++ src/components/common/Button/styles.ts
@@ -1,16 +1,29 @@
 import styled from '@emotion/styled';
 import { CommonButtonProps } from './types';
 
+const small = (p: any) => p.theme.breakpoints.small;
+
 export const BaseButton = styled.button<CommonButtonProps>`
   cursor: pointer;
   position: absolute;
   background: none;
   border: none;
-  height: 1.5rem;
-  width: 1.5rem;
+  height: 1.25rem;
+  width: 1.25rem;
 
-  :disabled {
+  &:disabled {
     visibility: hidden;
+  }
+
+  &:after {
+    display: inline-block;
+    height: 100%;
+    width: 100%;
+  }
+
+  @media (min-width: ${small}px) {
+    height: 1.5rem;
+    width: 1.5rem;
   }
 `;
 export const MarkTaskButton = styled(BaseButton)<{ task_confirmed?: boolean }>`
@@ -19,8 +32,17 @@ export const MarkTaskButton = styled(BaseButton)<{ task_confirmed?: boolean }>`
   &::after {
     content: ${(p) =>
       p.task_confirmed
-        ? `url('/assets/check-l.svg')`
-        : `url('/assets/check-d.svg')`};
+        ? `url('/assets/check-l-mobile.svg')`
+        : `url('/assets/check-d-mobile.svg')`};
+  }
+
+  @media (min-width: ${small}px) {
+    &::after {
+      content: ${(p) =>
+        p.task_confirmed
+          ? `url('/assets/check-l.svg')`
+          : `url('/assets/check-d.svg')`};
+    }
   }
 `;
 
@@ -28,9 +50,12 @@ export const DoneBg = styled.div<{
   task_confirmed?: boolean;
   disabled?: boolean;
 }>`
-  background-color: ${(p) => (p.task_confirmed ? '#007bff' : '#ffffff')};
-  height: 1.5rem;
-  width: 1.5rem;
+  background-color: ${(p) =>
+    p.task_confirmed
+      ? p.theme.colors.toDoListPage.bgButtonColor
+      : p.theme.colors.white};
+  height: 1.4rem;
+  width: 1.4rem;
   border-radius: 0.5rem;
   position: absolute;
   top: 0;
@@ -47,13 +72,30 @@ export const DoneBg = styled.div<{
   `}
 
   &:hover {
-    background-color: ${(p) => (p.task_confirmed ? '#ffffff' : '#007bff')};
+    background-color: ${(p) =>
+      p.task_confirmed
+        ? p.theme.colors.white
+        : p.theme.colors.toDoListPage.bgButtonColor};
 
     & ${MarkTaskButton}::after {
       content: ${(p) =>
         p.task_confirmed
-          ? `url('/assets/check-d.svg')`
-          : `url('/assets/check-l.svg')`};
+          ? `url('/assets/check-d-mobile.svg')`
+          : `url('/assets/check-l-mobile.svg')`};
+    }
+  }
+
+  @media (min-width: ${small}px) {
+    height: 1.5rem;
+    width: 1.5rem;
+
+    &:hover {
+      & ${MarkTaskButton}::after {
+        content: ${(p) =>
+          p.task_confirmed
+            ? `url('/assets/check-d.svg')`
+            : `url('/assets/check-l.svg')`};
+      }
     }
   }
 `;
@@ -61,10 +103,18 @@ export const DoneBg = styled.div<{
 export const ExcludeButton = styled(BaseButton)`
   right: 0;
   bottom: 0;
-  transform: translate(-46%, -25%);
+  transform: translate(-55%, -30%);
 
   &::after {
-    content: url('/assets/trash.svg');
+    content: url('/assets/trash-mobile.svg');
+  }
+
+  @media (min-width: ${small}px) {
+    transform: translate(-46%, -25%);
+
+    &::after {
+      content: url('/assets/trash.svg');
+    }
   }
 `;
 
@@ -72,6 +122,12 @@ export const EditTask = styled(BaseButton)`
   left: 0.5rem;
 
   &::after {
-    content: url('/assets/edit.svg');
+    content: url('/assets/edit-mobile.svg');
+  }
+
+  @media (min-width: ${small}px) {
+    &::after {
+      content: url('/assets/edit.svg');
+    }
   }
 `;
